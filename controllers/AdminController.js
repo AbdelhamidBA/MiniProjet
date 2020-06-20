8bf5482f5b6ccbde38d9ce259e8ee57b67dcb30b
lahmerines7 <lahmerines7@gmail.comm>
2020-06-20 03:09:14 +0200

    elimination

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,6 +1,12 @@
 const express= require('express');
 const mongoose = require('mongoose');
 const Etudiant = require('../models/Etudiant');
+const Salle = require('../models/Salle')
+const Enseignant = require('../models/Enseignant')
+const Matiere = require('../models/Matiere')
+const Seance = require('../models/Seance')
+const Filiere = require('../models/Filiere')
+const Presence = require('../models/Presence')
 
 exports.AddStudent= async(req,res) => {
   
@@ -16,7 +22,11 @@ exports.AddStudent= async(req,res) => {
       });
    try {  
         const savedetudiant= await etudiant.save();
+        
         res.json(savedetudiant);
+        console.log('add vide',savedetudiant);
+        if (savedetudiant==null)
+       {console.log('add vide');} else{console.log('add non vide');}
 
   }catch(err){
      res.json({message: err})
@@ -54,7 +64,7 @@ exports.getEtudiant= async(req,res) => {
     res.json(etudiant);
     }catch(err){res.json({message:err})}
 }
-exports.deleteStudent= async(res,req) => {
+exports.deleteStudent= async(req,res) => {
   try{
     const removedstudent= await Etudiant.remove({_id:req.params.studentId});
     res.json(removedstudent);
@@ -62,3 +72,83 @@ exports.deleteStudent= async(res,req) => {
     res.json({message:err})
   }
 }
+
+
+
+exports.Elimination= async(req,res) => {
+  let ListeElimination=[];
+  let Matiere= await getMatiere(req.params.idmatiere);
+  console.log("matiere",Matiere);
+  let Minseance = Matiere.nbSeanceMin;
+  try{
+    let liste= await getEtudiant(req.params.id_filiere)  
+            for(let i=0;i<liste.length;i++){
+                  let countpres=await countPresence(liste[i]._id,req.params.idmatiere);
+                         if (countpres < Minseance )
+                          ListeElimination.push(liste[i]);
+                        
+                        }   
+
+                        console.log("elimination"+ListeElimination)
+  }catch(err){
+    res.json({message:err})
+
+  }
+}
+
+
+async function getMatiere(idMat) {
+  try {
+      let mat = await Matiere.findOne({ _id: idMat })
+      return mat
+  } catch (err) {
+      console.log('Matiere:' + err)
+  }
+}
+
+async function getFiliere(idFiliere) {
+  try {
+      let filiere = await Filiere.findOne({ _id: idFiliere })
+      return filiere
+  } catch (err) {
+      console.log('Filiere:' + err)
+  }
+}
+
+
+
+
+async function countPresence(idetud,idmat) {
+  try {
+      let count= await Presence.count({
+     id_etd: idetud,
+      id_seance: { "$in": await getSeanceofmatiere(idmat) }
+      });
+     
+      console.log('count'+count) ;
+      return count;
+  }catch(err){
+    console.log('presencecount'+err)
+  }
+}
+
+async function getSeanceofmatiere(idmat) {
+  try {
+      let seance = await Seance.findOne({ id_matiere: idmat })
+      return seance._id
+  } catch (err) {
+      console.log('Seance:' + err)
+  }
+}
+
+async function getEtudiant(id_fil) {
+  try {
+      let etd = await Etudiant.find({ id_filiere: id_fil })
+      return etd
+  } catch (err) {
+      console.log('Etudiant:' + err)
+  }
+}
+
+
+
